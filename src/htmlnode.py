4f20e502ba1af1e2b1b3085d87a14d2a6b9f341b
Thomas Vu <thomasvu.tech@gmail.com>
2026-08-07 23:44:38 -0700

    feat: implement LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -28,3 +28,26 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str | None,
+        value: str,
+        props: dict[str, str] | None = None,
+    ):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("LeafNode must have a value")
+
+        if self.tag is None and self.value:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
